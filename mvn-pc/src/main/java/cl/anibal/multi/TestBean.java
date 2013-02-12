bd703e4fc7b41ec94c7938328e2ff42eeeb5a78c
nigue <anibaldavor@gmail.com>
2013-02-12 13:44:28 -0300

    first commit

---
+++ mvn-pc/src/main/java/cl/anibal/multi/TestBean.java
@@ -0,0 +1,32 @@
+package cl.anibal.multi;
+
+public class TestBean {
+
+    private Integer edad = null;
+    private String nombre = null;
+    private Boolean cuenta = false;
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Boolean getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(Boolean cuenta) {
+        this.cuenta = cuenta;
+    }
+}
